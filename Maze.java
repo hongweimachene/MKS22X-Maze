b795d178afdff516605f834055b9e2e0169bbdfd
hongweimachene <hchen00@stuy.edu>
2019-02-28 11:36:56 -0500

    finished constructor

--- Maze.java
+++ Maze.java
@@ -7,12 +7,38 @@ public class Maze{
   public Maze(String filename){
     try {
       File file = new File(filename);
+      Scanner dimensions = new Scanner(file);
+      int rows = 0;
+      int cols = 0;
+      while (dimensions.hasNextLine()) {
+        String line = dimensions.nextLine();
+        rows++;
+        cols = line.length();
+      }
+      maze = new char[rows][cols];
       Scanner data = new Scanner(file);
+      int row = 0;
+      while (data.hasNextLine()){
+        String line = data.nextLine();
+        for (int i = 0; i < line.length(); i++){
+          maze[row][i] = line.charAt(i);
+        }
+        row++;
+      }
+      animate = false;
     } catch (FileNotFoundException e) {
       System.out.println("File not Found");
       System.exit(1);
     }
-    if (data.hasNextLine())
+  }
+
+  public void setAnimate(boolean b) {
+    animate = b;
+  }
+  
+  public void clearTerminal(){
+    //erase terminal, go to top left of screen.
+    System.out.println("\033[2J\033[1;1H");
   }
 
   private boolean validState(char[][] maze) {
@@ -25,5 +51,16 @@ public class Maze{
       }
     }
     return (start == 1 && end == 1);
+  }
+
+  public String toString() {
+    String s = "";
+    for (int i = 0; i < maze.length; i++) {
+      for (int j = 0; j < maze[i].length; j++) {
+        s+=maze[i][j];
+      }
+      s+="\n";
+    }
+    return s;
   }
 }
